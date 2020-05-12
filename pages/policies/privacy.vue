9157b996be2553aa66a2892681e080d8272a174a
Savas Vedova <savas@stormkit.io>
2020-05-12 18:06:06 +0300

    Add terms and privacy

---
+++ pages/policies/privacy.vue
@@ -0,0 +1,195 @@
+<template>
+  <main>
+    <h1>Privacy Policy</h1>
+    <h3>Last Updated: May 12th, 2020</h3>
+    <p>
+      We at Stormkit ("Stormkit," "we," "us," or "our") have created this
+      privacy policy (this "Privacy Policy") because we want you to know how
+      information you provide to us is used and shared. This Privacy Policy
+      relates to the information collection and use practices of Stormkit in
+      connection with our Service which is made available to you through our
+      Website (including mobile web) located at
+      <nuxt-link to="/">https://www.stormkit.io</nuxt-link> and our App (or
+      Platform) - which may be accessed via the Website or App.
+    </p>
+    <p>
+      By visiting our Website or using our App, you are agreeing to the terms of
+      this Privacy Policy and the accompanying
+      <nuxt-link to="/policies/terms">Terms of Service</nuxt-link>.
+    </p>
+    <p>
+      Capitalized terms not defined in this Privacy Policy shall have the
+      meaning set forth in our Terms of Service.
+    </p>
+    <h3>Scope of this policy</h3>
+    <p>
+      The Website and the App are operated by Stormkit in Switzerland (CH), and
+      complies with Swiss law. Please be aware that any information you provide
+      to us may be transferred to, processed, maintained, and used on computers,
+      servers, and systems located outside of your home country, where the data
+      protection laws may not be as protective as those in your jurisdiction.
+      Currently, our servers are located in Frankfurt, Germany. Nevertheless, we
+      hold the right to move or extend our servers elsewhere in the World. In
+      such cases, you will be notified by means that are easily accesible, such
+      as Emails, our website, Twitter or other Social Media channels.
+    </p>
+    <!--
+    <h3>EEA Individuals</h3>
+    <p>
+      Natural persons located in the European Economic Area ("EEA") should
+      review the General Data Protection Regulation ("GDPR") privacy notice for
+      their specific rights, remedies, and other GDPR/Privacy Shield-related
+      information <a href="https://Stormkit.io/policies/gdpr">here</a>.
+    </p>
+    -->
+    <h3>Information We Collect and How We Use It</h3>
+    <ul>
+      <li><strong>Contact Information</strong></li>
+      <li><strong>Payment Information</strong></li>
+      <li><strong>From Your Activity</strong></li>
+      <li><strong>From Cookies</strong></li>
+    </ul>
+    <h3>Third-Party Analytics</h3>
+    <p>
+      We use one or more third–party analytics services (such as Google
+      Analytics) to evaluate your use of the Website, and the Platform, compile
+      reports on activity (based on their collection of IP addresses, Internet
+      service provider, browser type, operating system and language, referring
+      and exit pages and URLs, data and time, amount of time spent on particular
+      pages, what sections of the Website and/or the Platform you visit, number
+      of nuxt-links clicked while on the Website and/or the Platform, search
+      terms and other similar usage data), and analyze performance metrics.
+      These third parties use cookies and other technologies to help analyze and
+      provide us the data. By visiting the Website, and accessing and using the
+      Services and/or the Platform, you consent to the processing of data about
+      you by these analytics providers in the manner and for the purposes set
+      out in this Privacy Policy. For more information on these third parties,
+      including how to opt out from certain data collection, please visit the
+      sites below. Please be advised that if you opt out of any service, you may
+      not be able to use the full functionality of the Websites, Service or the
+      Platform.
+    </p>
+    <p>
+      Google Analytics:
+      <a
+        href="https://www.google.com/analytics"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        https://www.google.com/analytics
+      </a>
+    </p>
+    <p>
+      Hotjar:
+      <a
+        href="https://www.hotjar.com/"
+        rel="noreferrer noopener"
+        target="_blank"
+        >https://www.hotjar.com</a
+      >
+    </p>
+    <h3>CRM Data</h3>
+    <p>
+      We store your contact information within our HubSpot to help our sales and
+      customer support team manage our relationships with current or prospective
+      customers. Any personal data provided to/or collected by HubSpot will be
+      used to provide our services in accordance with this Privacy Policy.
+    </p>
+    <h3>Business Transfers</h3>
+    <p>
+      In the event of a merger, dissolution or similar corporate event, or the
+      sale of all or substantially all of our assets, we expect that the
+      information that we have collected, including personal information, would
+      be transferred to the surviving entity in a merger or the acquiring
+      entity. All such transfers shall be subject to our commitments with
+      respect to the privacy and confidentiality of such personal information as
+      set forth in this Privacy Policy.
+    </p>
+    <h3>Disclosure to Public Authorities</h3>
+    <p>
+      We are required to disclose personal information in response to lawful
+      requests by public authorities, including for the purpose of meeting
+      national security or law enforcement requirements. We may also disclose
+      personal information to other third parties when compelled to do so by
+      government authorities or required by law or regulation including, but not
+      limited to, in response to court orders and subpoenas.
+    </p>
+    <h3>Opt Out for Direct Marketing</h3>
+    <p>
+      You may opt out at any time from the use of your personal information for
+      direct marketing purposes, and/or transfer of your personal information to
+      third parties for direct marketing purposes by sending us an email at
+      <a href="mailto:privacy@stormkit.io">privacy@stormkit.io</a>. Please allow
+      us a reasonable time to process your request. You cannot opt out of
+      receiving transactional or administrative e-mails related to our services
+      or your requests for information or materials.
+    </p>
+    <h3>Retention of personal information</h3>
+    <p>
+      We will retain your personal information in a form that identifies you
+      only for as long as it serves the purpose(s) for which it was initially
+      collected as stated in this Privacy Policy, subsequently authorized, or as
+      allowed under applicable law.
+    </p>
+    <h3>How We Protect Your Information</h3>
+    <p>
+      We take very seriously the security and privacy of the personal
+      information that it collects pursuant to this Privacy Policy. Accordingly,
+      we will implement reasonable and appropriate security measures to protect
+      your personal information from loss, misuse and unauthorized access,
+      disclosure, alteration and destruction, taking into account the risks
+      involved in processing and the nature of such data, and comply with
+      applicable laws and regulations.
+    </p>
+    <h3>Children</h3>
+    <p>
+      We do not knowingly collect personal information from children under the
+      age of 13 through the Website or the Service. If you are under 13, please
+      do not give us any personal information. We encourage parents and legal
+      guardians to monitor their children's Internet usage and to help enforce
+      our Privacy Policy by instructing their children to never provide us
+      Personal Information without their permission. If you have reason to
+      believe that a child under the age of 13 has provided personal information
+      to us, please contact us, and we will endeavor to delete that personal
+      information from our databases.
+    </p>
+    <h3>External Websites</h3>
+    <p>
+      The Website may contain nuxt-links to third party websites. Stormkit has
+      no control over the privacy practices or the content of any of our
+      business partners, advertisers, sponsors, or other websites to which we
+      provide nuxt-links. As such, we are not responsible for the content or the
+      privacy policies of those third party websites. You should check the
+      applicable third party privacy policy and terms of use when visiting any
+      other websites.
+    </p>
+    <h3>Changes to This Privacy Policy</h3>
+    <p>
+      This Privacy Policy is effective as of the date stated at the top of this
+      Privacy Policy. We may change this Privacy Policy from time to time, and
+      will post any changes on the Website or notify you via e-mail, at our
+      option, as soon as they go into effect. By using the Website, App or the
+      Service after we make any such changes to this Privacy Policy, you are
+      deemed to have accepted such changes. Please refer back to this Privacy
+      Policy on a regular basis.
+    </p>
+    <h3>Ownership</h3>
+    <p>
+      We claim no ownership over the code shared with us. Your repository or
+      repositories (hosted on services such as Github, Bitbucket or Gitlab) are
+      solely yours and are used only to deploy your application and thrown away
+      as soon as the deployment is completed. We have no intention to change
+      this behaviour.
+    </p>
+    <h3>How to Contact Us</h3>
+    <p>
+      If you have questions about this Privacy Policy, please e-mail us at
+      <a href="mailto:privacy@Stormkit.io">privacy@stormkit.io</a>.
+    </p>
+  </main>
+</template>
+<script>
+export default {
+  layout: 'policies'
+}
+</script>

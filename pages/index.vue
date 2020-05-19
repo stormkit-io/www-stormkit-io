d136a7a26dfdb6770c068e47c57682070fb30c96
Savas Vedova <savas@stormkit.io>
2020-05-19 10:25:56 +0300

    Create about us section

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,23 @@
   <div
     class="flex flex-col justify-center min-h-screen min-w-screen relative overflow-hidden"
   >
-    <sk-welcome />
-    <div class="page md:mt-24 p-3 md:p-0">
+    <div class="md:mb-24 md:p-0">
+      <sk-welcome />
+    </div>
+    <div class="mb-12 md:mb-24 md:p-0">
       <sk-features />
     </div>
-    <div class="mt-16 p-6 md:mt-24 p-3 md:p-0">
+    <div class="mb-12 md:mb-24 md:p-0">
       <sk-testimonials />
     </div>
-    <div class="mb-12 md:my-24 p-6 md:p-0">
+    <div class="mb-12 md:mb-24 md:p-0">
       <sk-why-when />
     </div>
-    <div class="mb-12 md:my-24 p-6 md:p-0">
+    <div class="mb-12 md:mb-24 md:p-0">
       <sk-supported-techs />
+    </div>
+    <div class="mb-12 md:mb-24 md:p-0">
+      <sk-team />
     </div>
   </div>
 </template>
@@ -24,6 +29,7 @@ import SkFeatures from './-index-features'
 import SkTestimonials from './-index-testimonials'
 import SkWhyWhen from './-index-why-when'
 import SkSupportedTechs from './-index-supported-techs'
+import SkTeam from './-index-team'
 
 export default {
   components: {
@@ -31,7 +37,8 @@ export default {
     SkFeatures,
     SkTestimonials,
     SkWhyWhen,
-    SkSupportedTechs
+    SkSupportedTechs,
+    SkTeam
   }
 }
 </script>

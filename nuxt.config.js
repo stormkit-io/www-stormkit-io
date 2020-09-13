aab6f0b25670df5b3af0b3d9bec1db3ae65dbce1
Savas Vedova <savas@vsk.me>
2020-09-13 20:46:23 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: 'Stormkit - Serverless infrastructure for javascript applications',
+    title: 'Stormkit - Serverless infrastructure for modern javascript applications',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

86d4a401eb28d262dbf61c40f4d3f7ffec8751b2
Savas Vedova <savas@stormkit.io>
2023-05-22 10:52:47 +0300

    chore: add social links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import { useTheme } from '@mui/material/styles'
 import IconButton from '@mui/material/IconButton'
 import Box from '@mui/material/Box'
-import { useTheme } from '@mui/material/styles'
 import Link from '@mui/material/Link'
+import { ArrowRightAlt, Menu, Close, Twitter } from '@mui/icons-material'
 import StormkitLogoText from '~/assets/logos/stormkit-logo-text-h--white.svg'
-import { ArrowRightAlt, Menu, Close } from '@mui/icons-material'
+import DiscordLogo from '~/assets/images/discord.svg'
 
 interface Props {
   maxWidth?: string
@@ -17,6 +18,41 @@ const links = [
   { path: '/docs', text: 'docs' },
   { path: '/blog', text: 'blog' },
   { path: '/blog/whats-new', text: 'whats new?' },
+  {
+    path: 'https://twitter.com/stormkitio',
+    text: (
+      <>
+        <Twitter sx={{ mr: 0, fill: 'rgb(29, 155, 240)' }} />
+        <Box
+          component="span"
+          sx={{ display: { xs: 'inline', md: 'none' }, ml: { xs: 1, md: 0 } }}
+        >
+          Twitter
+        </Box>
+      </>
+    ),
+    separator: true,
+  },
+  {
+    path: 'https://discord.com/invite/6yQWhyY',
+    text: '',
+    icon: (
+      <>
+        <Box
+          component="img"
+          src={DiscordLogo}
+          alt={'Discord'}
+          sx={{ mr: 1, display: 'inline-block', width: '23px' }}
+        />
+        <Box
+          component="span"
+          sx={{ display: { xs: 'inline', md: 'none' }, ml: { xs: 0.5, md: 0 } }}
+        >
+          Discord
+        </Box>
+      </>
+    ),
+  },
   {
     path: 'https://app.stormkit.io',
     text: 'login',

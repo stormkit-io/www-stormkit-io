0839597fc22ef400f8df70dd5aea4e5e50191f3a
Savas Vedova <savas@stormkit.io>
2023-08-19 20:58:22 +0300

    fix: several issues with vite

--- vite.config.ssr.ts
+++ vite.config.ssr.ts
@@ -44,6 +44,20 @@ export default defineConfig({
         preserveModulesRoot: 'src',
         exports: 'named',
       },
+      // Material ui's "use client" directive causes a warning.
+      // This function ignores those warnings.
+      // See https://github.com/rollup/rollup/issues/4699#issuecomment-1571555307
+      // for more information.
+      onwarn(warning, warn) {
+        if (
+          warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+          warning.message.includes('use client')
+        ) {
+          return
+        }
+
+        warn(warning)
+      },
     },
   },
   define: {
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,20 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       input: 'index.html',
+      // Material ui's "use client" directive causes a warning.
+      // This function ignores those warnings.
+      // See https://github.com/rollup/rollup/issues/4699#issuecomment-1571555307
+      // for more information.
+      onwarn(warning, warn) {
+        if (
+          warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+          warning.message.includes('use client')
+        ) {
+          return
+        }
+
+        warn(warning)
+      },
     },
     outDir: '.stormkit/public',
   },

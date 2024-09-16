2199461c488b1d23bee8a546e382c7b4bdb3a506
Savas Vedova <savas@stormkit.io>
2024-09-16 16:40:37 +0300

    chore: create an install file

---
+++ public/scripts/install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+
+# Determine the machine
+case "${unameOut}" in
+  Linux*)     machine=Linux;;
+  Darwin*)    machine=Mac;;
+  CYGWIN*)    machine=Cygwin;;
+  MINGW*)     machine=MinGw;;
+  *)          machine="UNKNOWN:${unameOut}"
+esac
+
+# Function to check if a command exists
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+if [ "$machine" == "Mac" ]; then
+  if command_exists brew; then
+    echo "Brew already installed"
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  if command_exists docker; then
+    echo "Docker already installed"
+  else
+    brew install docker
+  fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # We have to run as root
+  if [ "$(id -u)" -ne 0 ]; then
+      echo "This script must be run as root" >&2
+      exit 1
+  fi
+
+  # Check if ports 80 or 443 are in use
+  for port in 80 443; do
+      if ss -tulnp | grep ":${port} " >/dev/null; then
+          echo "Port ${port} is already in use" >&2
+          exit 1
+      fi
+  done
+
+  # Install Docker if not already installed
+  if command_exists docker; then
+      echo "Docker already installed"
+  else
+      curl -sSL https://get.docker.com | sh
+  fi
+fi
+
+# Go to home directory
+cd
+
+mkdir -p stormkit
+
+# Download the docker-compose.yaml file
+curl -o "docker-compose.yaml" "https://raw.githubusercontent.com/stormkit-io/bin/main/docker-compose.yaml"
+
+# Download the example .env file
+curl -o ".env" "https://raw.githubusercontent.com/stormkit-io/bin/main/.env.example"
+
+# Leave Docker Swarm if initialized
+docker swarm leave --force 2>/dev/null
+
+# Initialize stack (only needed for master node)
+docker swarm init
+
+# Deploy the stack
+docker compose config | sed '/published:/ s/"//g' | sed "/name:/d" | docker stack deploy -c - stormkit
+
+# Define text colors for output
+GREEN="\033[0;32m"
+NC="\033[0m"
+
+echo ""
+printf "${GREEN}Congratulations, Stormkit is installed on your computer!${NC}\n"
+echo ""

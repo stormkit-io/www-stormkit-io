b9cfaf9f1e9106db256039ff5a2c5a53c95e7aaa
Savas Vedova <savas@stormkit.io>
2024-09-27 21:55:35 +0300

    chore: update serverless package

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,6 +2,7 @@ import { StaticRouter } from 'react-router-dom/server'
 import { renderToString } from 'react-dom/server'
 import fs from 'node:fs'
 import path from 'node:path'
+import http from 'node:http'
 import { fileURLToPath } from 'node:url'
 import serverless from '@stormkit/serverless'
 import createRoutes from './routes'
@@ -95,20 +96,22 @@ export const render: RenderFunction = async (url, seo) => {
 
 // This handler add support for Stormkit environment. This is
 // the entry point of the serverless application.
-export const handler = serverless(async (req: any, res: any) => {
-  // We are in assets folder
-  const dir = path.dirname(fileURLToPath(import.meta.url))
-  const html = fs.readFileSync(path.join(dir, './index.html'), 'utf-8')
+export const handler = serverless(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    // We are in assets folder
+    const dir = path.dirname(fileURLToPath(import.meta.url))
+    const html = fs.readFileSync(path.join(dir, './index.html'), 'utf-8')
 
-  const { status, content, head } = await render(
-    req.url?.split(/\?#/)[0] || '/'
-  )
+    const { status, content, head } = await render(
+      req.url?.split(/\?#/)[0] || '/'
+    )
 
-  res.writeHead(status, 'OK', { 'Content-Type': 'text/html; charset=utf-8' })
-  res.write(
-    html
-      .replace('</head>', `${head}</head>`)
-      .replace(`<div id="root"></div>`, `<div id="root">${content}</div>`)
-  )
-  res.end()
-})
+    res.writeHead(status, 'OK', { 'Content-Type': 'text/html; charset=utf-8' })
+    res.write(
+      html
+        .replace('</head>', `${head}</head>`)
+        .replace(`<div id="root"></div>`, `<div id="root">${content}</div>`)
+    )
+    res.end()
+  }
+)
